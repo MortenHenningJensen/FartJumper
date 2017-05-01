47a94497afecf2a3030166953e6c34d188a67636
Morten Jensen <mortenhenningjensen@hotmail.com>
2017-05-01 17:50:24 +0200

    Here we go!

---
+++ Fart Jumper/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public Rigidbody myBody;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.AddForce(5, 5, 0, ForceMode.Impulse);
+        }
+	}
+}
